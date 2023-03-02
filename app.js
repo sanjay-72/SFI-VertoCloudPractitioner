bc2ce0f5bcd6fbfcc2ef9c46d837aa4fd3c5a16e
Sanjay Kumar Konakandla <sanjaykumarkonakandla@gmail.com>
2023-03-02 06:33:39 +0530

    Sample Form and Server Check

---
+++ app.js
@@ -0,0 +1,21 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+require('dotenv').config();
+app.use(bodyParser.urlencoded({ extended: true }));
+const PORT = 80;
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post("/", function (req, res) {
+    console.log(req.body);
+    res.send("Thankyou");
+});
+
+app.listen(PORT, function () {
+    console.log("App is running on port : " + PORT);
+});
