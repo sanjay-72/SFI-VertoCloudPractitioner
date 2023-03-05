f95f5e53864649a264a26f5a988e70170086e529
Sanjay Kumar Konakandla <sanjaykumarkonakandla@gmail.com>
2023-03-05 21:00:59 +0530

    Added login page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = 80;
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -14,6 +15,10 @@ app.get("/", function (req, res) {
 app.post("/", function (req, res) {
     console.log(req.body);
     res.send("Thankyou");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login", {});
 });
 
 app.listen(PORT, function () {
