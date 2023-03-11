55e2b4194fa92c0c16bea32624efa222297008fc
Sanjay Kumar Konakandla <sanjaykumarkonakandla@gmail.com>
2023-03-11 15:28:40 +0530

    Custom images for products

--- public/marketPage.js
+++ public/marketPage.js
@@ -10,8 +10,8 @@ function showProducts(products) {
     products.forEach(product => {
         skillHTML += `
             <div class="myProductBox">
-            <img src="https://images.unsplash.com/photo-1584673961397-be26e009333f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Z3JlZW4lMjBmaWVsZHxlbnwwfHwwfHw%3D&w=1000&q=80"
-                alt="...">
+            <img src="images/${product.ProductName}.jpg"
+                alt="${product.ProductName} Image">
             <h2>${product.ProductName}</h2>
             <p class="breakAll narrow">${product.Description}</p>
             <a href="#"><button class="btn btn-outline-success">Contact Seller</button></a>
