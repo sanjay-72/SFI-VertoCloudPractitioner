45eed2d8308c4ad28fda7023a5cd8fe1e9ee2bfa
Sanjay Kumar Konakandla <sanjaykumarkonakandla@gmail.com>
2023-03-25 07:28:42 +0530

    nav toggle btn for home page

--- public/index.js
+++ public/index.js
@@ -10,3 +10,24 @@ VanillaTilt.init(document.querySelectorAll(".tilt"), {
     max: 20,
     speed: 400
 });
+
+//0 is unclicked and 1 is clicked
+let btnState = 0;
+homeBtn = document.getElementById("homeBtn");
+iotBtn = document.getElementById("agriIOTBtn");
+marketBtn = document.getElementById("marketBtn");
+aboutBtn = document.getElementById("aboutBtn");
+loginBtn = document.getElementById("loginBtn");
+document.getElementById("toggle-button").addEventListener("click", () => {
+    homeBtn.classList.toggle("hide");
+    iotBtn.classList.toggle("hide");
+    marketBtn.classList.toggle("hide");
+    aboutBtn.classList.toggle("hide");
+    loginBtn.classList.toggle("hide");
+    if (btnState == 1) {
+        document.getElementById("toggle-button").blur();
+        btnState = 0;
+    }
+    else
+        btnState = 1;
+});
